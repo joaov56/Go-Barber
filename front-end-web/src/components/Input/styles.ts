da2895668a67ea1ab2f60bf01e39e3829b69087e
João Vitor <joao@MacBook-Pro-de-Mac-2.local>
2021-01-19 10:24:10 -0300

    desacoplando componentes

---
+++ front-end-web/src/components/Input/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #666360;
+  display: flex;
+  align-items: center;
+  & + div {
+    margin-top: 8px;
+  }
+  input {
+    background: transparent;
+    flex: 1;
+    border: none;
+    color: #f4ede8;
+    outline: none;
+    & + input {
+      margin-top: 8px;
+    }
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+  svg {
+    margin-right: 16px;
+  }
+`;
